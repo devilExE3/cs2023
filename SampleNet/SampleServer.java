afe5d3edf7630d6aa406a4ee24787b05c73a8a77
Florin.Teo <Florin.Teo@live.com>
2023-07-25 14:48:44 +0300

    More changes

--- SampleNet/SampleServer.java
+++ SampleNet/SampleServer.java
@@ -1,12 +1,14 @@
 package SampleNet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SampleServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println("Hello to SampleServer");
         InetAddress inetAddress = InetAddress.getLocalHost();
         System.out.println(inetAddress);
@@ -14,10 +16,16 @@ public class SampleServer {
         ServerSocket srvSocket = new ServerSocket(5025);
         System.out.println("Accepting message");
         System.out.println(srvSocket.getSoTimeout());
-        srvSocket.setSoTimeout(5000);
+        //srvSocket.setSoTimeout(5000);
         System.out.println(srvSocket.getSoTimeout());
         Socket socket = srvSocket.accept();
         System.out.println("Got a connection");
+
+        InputStream inStream = socket.getInputStream();
+        ObjectInputStream objInStream = new ObjectInputStream(inStream);
+
+        String inMessage = (String)objInStream.readObject();
+        System.out.println(inMessage);
 
         socket.close();
         srvSocket.close();
