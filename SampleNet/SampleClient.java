e74cd3c1ea92a2411aa2f3ecaf49352aa5d46aae
Florin.Teo <Florin.Teo@live.com>
2023-07-25 14:48:36 +0300

    Updates

--- SampleNet/SampleClient.java
+++ SampleNet/SampleClient.java
@@ -1,6 +1,8 @@
 package SampleNet;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -11,8 +13,16 @@ public class SampleClient {
         InetAddress inetAddress = InetAddress.getLocalHost();
         System.out.println(inetAddress);
         System.out.println("Connecting");
-        Socket socket = new Socket(inetAddress, 5025);
+        Socket socket = new Socket("192.168.5.157", 5025);
         System.out.println("Connected");
+
+        OutputStream outStr = socket.getOutputStream();
+        ObjectOutputStream objOutStream = new ObjectOutputStream(outStr);
+
+        String myMessage = "Hello from Client!";
+        System.out.println(myMessage);
+        objOutStream.writeObject(myMessage);
+        
         
         socket.close();
     } 
