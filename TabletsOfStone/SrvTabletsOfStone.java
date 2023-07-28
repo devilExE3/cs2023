9d9c2ee2b10030fab11a38a47505464444f6b316
devilExE3 <devilexe1337@gmail.com>
2023-07-28 09:27:40 +0300

    error when message is over 6 characters long

--- TabletsOfStone/SrvTabletsOfStone.java
+++ TabletsOfStone/SrvTabletsOfStone.java
@@ -155,7 +155,7 @@ public class SrvTabletsOfStone {
         
         Queue<MsgTabletsOfStone> msgQueue = _msgQueues.get(to);
         if (message.getData().length() > 6)
-            message = new MsgTabletsOfStone(message.getFrom(), message.getTo(), message.getData().substring(0, 6));
+            return new MsgTabletsOfStone(0, "[Err] Invalid message length!");
         msgQueue.add(message);
         return new MsgTabletsOfStone(0, "[Success] OK!");
     }
